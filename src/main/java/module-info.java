73b1b496ea0ea610f2ed5c2a238fb2e16c04c13a
Priyanka Sarkate <priyankaaasarkate@gmail.com>
2023-02-22 15:45:15 -0500

    pom.xml for java swing

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,6 @@
 module com.pitt.visualization {
-    requires javafx.controls;
-    requires javafx.fxml;
-	requires com.fasterxml.jackson.databind;
-	requires formats.api;
-	requires java.desktop;
+	requires javafx.fxml;
+	requires javafx.graphics;
 	requires javafx.swing;
-
-    opens com.pitt.visualization to javafx.fxml;
-    exports com.pitt.visualization;
+	requires formats.api;
 }
